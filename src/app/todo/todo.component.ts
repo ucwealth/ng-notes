8be4a0b0db37974cfe95293b0bede0a1d04fc86d
ucwealth <ucheokeke16@gmail.com>
2020-05-31 21:48:05 +0100

    edited todo view

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-
+public name = "Uche";
+public siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  greetUser(){
+    return "Welcome " + this.name;
   }
 
 }
