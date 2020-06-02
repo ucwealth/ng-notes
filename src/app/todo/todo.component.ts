187787e21a566a52c49d4defe4ff610c879f58db
ucwealth <ucheokeke16@gmail.com>
2020-06-02 15:15:59 +0100

    small changes

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,11 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
-public name = "Uche";
+public name = "";
 public siteUrl = window.location.href;
 public myId = "todoId";
 public isDisabled = true;
 public successClass = "text-success";
+public hasError = true;
+public isSpecial = true;
+public msgClasses = {
+  "text-success": !this.hasError,
+  "text-danger": this.hasError,
+  "text-special": this.isSpecial
+
+}
   constructor() { }
 
   ngOnInit() {
